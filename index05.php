1225c74c7c955ffdea69ec566e9b31ae50e08fa4
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-10 04:48:00 +0000

    文字列の連結を学んだので記録

---
+++ index05.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Progate</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>
+  
+    <?php
+      $name = 'にんじゃわんこ';
+      // 'こんにちは！'という文字列と$nameを連結してechoしてください
+      // ダブルクォーテーションじゃないと変数名のまま出力されてしまう
+      echo "こんにちは！{$name}";
+      
+    ?>
+  
+  </body>
+</html>
