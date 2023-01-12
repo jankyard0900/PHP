5c248f23b6e11729ddce1b7d3e5d304796662a95
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-12 06:32:55 +0000

    for文の書き方と簡単なプログラムを記述しました。記録するため

---
+++ for01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Progate</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>
+  
+    <?php
+  
+      // この下にfor文を書いてください
+      for ($i = 51; $i <= 100; $i++) {
+        echo $i; 
+        // 出力結果を改行するため
+        echo '<br>';
+      }
+      
+    ?>
+  
+  </body>
+</html>
