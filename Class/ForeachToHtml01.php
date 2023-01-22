55e2b9bf09dac5b18a011fd59be991b7bbbc5e5b
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2023-01-22 07:04:22 +0000

    注文サービスのフォームを作成

--- Class/ForeachToHtml01.php
+++ Class/ForeachToHtml01.php
@@ -14,20 +14,27 @@ require_once('data.php');
 <body>
   <div class="menu-wrapper container">
     <h1 class="logo">Café Progate</h1>
-    <div class="menu-items">
-      <!-- 配列$menusの要素を変数$menuとするforeach文を書いてください -->
-      <?php foreach ($menus as $menu): ?>
-        <div class="menu-item">
-          <!-- <img>タグのsrc属性に、$menuのimageプロパティを表示してください -->
-          <img src="<?php echo $menu->getImage(); ?>">
-          <h3 class="menu-item-name"><?php echo $menu->getName(); ?></h3>
-          <!-- <p>タグの中に、$menuのpriceプロパティを表示してください -->
-          <p class="price">¥<?php echo $menu->getTaxIncludedPrice(); ?></p>
-          <!-- $menuのゲッターを用いてorderCountプロパティを表示してください -->
-          <p>注文数: <?php echo $menu->getOrderCount(); ?></p>
-        </div>
-      <?php endforeach ?>
-    </div>
+    <!-- ここに<form>の開始タグを書いてください -->
+    <form method="post" action="confirm.php">
+      <div class="menu-items">
+        <!-- 配列$menusの要素を変数$menuとするforeach文を書いてください -->
+        <?php foreach ($menus as $menu): ?>
+          <div class="menu-item">
+            <!-- <img>タグのsrc属性に、$menuのimageプロパティを表示してください -->
+            <img src="<?php echo $menu->getImage(); ?>">
+            <h3 class="menu-item-name"><?php echo $menu->getName(); ?></h3>
+            <!-- <p>タグの中に、$menuのpriceプロパティを表示してください -->
+            <p class="price">¥<?php echo $menu->getTaxIncludedPrice(); ?></p>
+            <!-- <input>タグを用いて入力ボックスを作成してください  -->
+            <input type="text" value="0" name="<?php echo $menu->getName(); ?>">
+            <span>個</span>
+          </div>
+        <?php endforeach ?>
+      </div>
+      <!-- <input>タグを用いて送信ボタンを作成してください  -->
+      <input type="submit" value="注文する">
+    <!-- ここで<form>の閉じタグを書いてください -->
+    </form>
   </div>
 </body>
 </html>
